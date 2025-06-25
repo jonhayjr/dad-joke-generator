9cab356fbfbb3516f2c95199c8062be6350cf3a0
jonhayjr <jonhayjr@gmail.com>
2025-06-25 06:25:55 -0600

    Created array of jokes

---
+++ script.js
@@ -0,0 +1,26 @@
+const jokes = [
+    {joke: 'What does a baby computer call his father?',
+     answer: 'Data.'
+    },
+    {joke: 'After an unsuccessful harvest, why did the farmer decide to try a career in music?',
+     answer: 'Because he had a ton of sick beets.'
+    },
+    {joke: 'I only seem to get sick on weekdays.',
+     answer: 'I must have a weekend immune system.'
+    },
+    {joke: 'My friend was showing me his tool shed and pointed to a ladder.',
+     answer: '"That\'s my stepladder," he said. "I never knew my real ladder."'
+    },
+    {joke: '"This morning, Siri said, "Don\'t call me Shirley."',
+     answer: "I accidentally left my phone in Airplane mode."
+    },
+    {joke: 'Which days are the strongest? Saturday and Sunday.',
+     answer: 'The rest are weekdays.'
+    },
+    {joke: 'If I ever find the doctor who screwed up my limb replacement surgery…',
+     answer: 'I\'ll kill him with my bear hands.'
+    },
+    {joke: 'I asked my date to meet me at the gym but she never showed up.',
+     answer: 'I guess the two of us aren\'t going to work out.'
+    }
+]
